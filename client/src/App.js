fcb37ea435b59927039af7a21bece6e85cd1bca7
Blanco237 <asongrandy9@gmail.com>
2022-08-06 21:25:18 +0100

    Updated Project

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,11 @@
-import React, { useEffect, useState } from 'react';
-import  {BrowserRouter as Router, Route } from 'react-router-dom';
-import Header from './components/Header';
-import Home from './views/Home';
+import React from 'react';
+import AppRouter from './routes/AppRouter';
 
 const App = () => {
 
     return (
         <>
-            <Header />
-            <Home />
+            <AppRouter />
         </>
     )
 }
