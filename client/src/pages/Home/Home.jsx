52c8c72f17072cbc488bfa53ccc32e55e6c622d3
Blanco237 <asongrandy9@gmail.com>
2022-08-11 15:46:06 +0100

    Updates

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import Carousel from '../../components/home/Carousel/Carousel'
 
 import styles from './home.module.css'
 
 const Home = () => {
     return (
         <div className={styles.body}>
-            
+            <Carousel />
         </div>
     )
 }
