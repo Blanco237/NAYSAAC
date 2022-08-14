8e9c77947cbee6fffd0d8e7673d26c3364dcffde
Blanco237 <asongrandy9@gmail.com>
2022-08-14 17:29:51 +0100

    Completed Home

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,5 +1,11 @@
 import React from 'react'
+import About from '../../components/home/About/About'
 import Carousel from '../../components/home/Carousel/Carousel'
+import SectionOne from '../../components/home/SectionOne/SectionOne'
+import Statistics from '../../components/home/Statistics/Statistics'
+import SDGS from '../../components/home/SDGS/SDGS'
+import Testimonials from '../../components/home/Testimonials/Testimonials'
+import Blogs from '../../components/home/Blogs/Blogs'
 
 import styles from './home.module.css'
 
@@ -7,6 +13,12 @@ const Home = () => {
     return (
         <div className={styles.body}>
             <Carousel />
+            <SectionOne />
+            <About />
+            <Statistics />
+            <SDGS />
+            <Testimonials />
+            <Blogs />
         </div>
     )
 }
